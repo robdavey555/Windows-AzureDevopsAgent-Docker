320885577109f311eb3759f63a3f2141530dd001
Rob Davey <robdavey555@hotmail.com>
2022-02-13 16:10:51 +0000

    update ps files

--- VMPowershell/StartAgenOnAzureVM.ps1
+++ VMPowershell/StartAgenOnAzureVM.ps1
@@ -1,25 +1,32 @@
+# AZP_URL=https://dev.azure.com/hrlocker 
+# AZP_TOKEN=c6oqkpixp2wurqf4vtwguuofgrxf4bsw45pxfhmm2zrybscoki2a
+# AZP_POOL="Test Complete Agent Pool" 
+# AZP_AGENT_NAME=testcompleteagent1
+
+
+$Env:AZP_URL="https://dev.azure.com/hrlocker" 
+$Env:AZP_AGENT_NAME="testcompleteagent1"
+$Env:AZP_POOL="Test Complete Agent Pool"
+$Env:AZP_TOKEN="c6oqkpixp2wurqf4vtwguuofgrxf4bsw45pxfhmm2zrybscoki2a"
+
+$Env:AZP_UserName="testcomepletepoc"
+$Env:AZP_UserPassword="urqf4vtwgu1X"
+
 if (-not (Test-Path Env:AZP_URL)) {
   Write-Error "error: missing AZP_URL environment variable"
   exit 1
 }
 
-if (-not (Test-Path Env:AZP_TOKEN_FILE)) {
-  if (-not (Test-Path Env:AZP_TOKEN)) {
-    Write-Error "error: missing AZP_TOKEN environment variable"
-    exit 1
-  }
-
-  $Env:AZP_TOKEN_FILE = "\azp\.token"
+$Env:AZP_TOKEN_FILE = "\azp\.token"
   $Env:AZP_TOKEN | Out-File -FilePath $Env:AZP_TOKEN_FILE
-}
 
 Remove-Item Env:AZP_TOKEN
 
 if ((Test-Path Env:AZP_WORK) -and -not (Test-Path $Env:AZP_WORK)) {
-  New-Item $Env:AZP_WORK -ItemType directory | Out-Null
+  New-Item $Env:AZP_WORK -ItemType directory -Force | Out-Null
 }
 
-New-Item "\azp\agent" -ItemType directory | Out-Null
+New-Item "\azp\agent" -ItemType directory -Force | Out-Null
 
 # Let the agent ignore the token env variables
 $Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
@@ -52,6 +59,9 @@ try {
     --token "$(Get-Content ${Env:AZP_TOKEN_FILE})" `
     --pool "$(if (Test-Path Env:AZP_POOL) { ${Env:AZP_POOL} } else { 'Default' })" `
     --work "$(if (Test-Path Env:AZP_WORK) { ${Env:AZP_WORK} } else { '_work' })" `
+    --runAsAutoLogon `
+    --windowsLogonAccount "$(${Env:AZP_UserName})" `
+    --windowsLogonPassword "$(${Env:AZP_UserPassword})" `
     --replace 
 
   Write-Host "4. Running Azure Pipelines agent..." -ForegroundColor Cyan
@@ -65,3 +75,5 @@ finally {
     --auth PAT `
     --token "$(Get-Content ${Env:AZP_TOKEN_FILE})"
 }
+
+
